2619daa7ca13b1306b5fed9a73eefe3421cf38ee
Viktor Illmer <1476338+v-ji@users.noreply.github.com>
2024-06-26 14:21:24 +0200

    Change Segment styling

--- src/lib/components/Segment.svelte
+++ src/lib/components/Segment.svelte
@@ -9,9 +9,9 @@
 </script>
 
 <div class="segment">
-  <h4>{segmentId}</h4>
   <!-- eslint-disable-next-line svelte/no-at-html-tags -->
   {@html segments[segmentId]}
+  <span class="number">{segmentId}</span>
 </div>
 
 <style>
@@ -24,5 +24,41 @@
     box-shadow:
       0 4px 6px -1px rgb(0 0 0 / 0.1),
       0 2px 4px -2px rgb(0 0 0 / 0.1);
+  }
+
+  .number {
+    display: block;
+    text-align: center;
+    opacity: 0.5;
+    font-size: small;
+    font-weight: normal;
+    margin-top: 1rem;
+  }
+
+  :global([subtype*="manicule-left"] + .number:after) {
+    content: " ☚";
+  }
+  :global([subtype*="manicule-right"] + .number:before) {
+    content: "☛ ";
+  }
+
+  /* TEI styles */
+  :global(.tei-speaker) {
+    font-weight: bold;
+    /* Sperrsatz */
+    letter-spacing: 0.1em;
+  }
+  :global(.tei-stage) {
+    font-style: italic;
+  }
+  :global(.tei-sp) {
+    --indent: 1.5em;
+    text-indent: calc(var(--indent) * -1);
+    padding-inline-start: var(--indent);
+  }
+  :global(ul.tei-castList) {
+    list-style: none;
+    padding: 0;
+    margin-top: 0;
   }
 </style>
