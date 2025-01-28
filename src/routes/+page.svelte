2701a70d12f9b32581060dc45405637832d6a5de
Viktor Illmer <1476338+v-ji@users.noreply.github.com>
2025-01-28 15:51:26 +0100

    Migrate styles to Tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,53 +34,44 @@
   }
 </script>
 
-<h1>
-  Neunhundert neun und neunzig <br /> <small>und noch etliche</small> <br /> Almanachs-Lustspiele
-  <br /> <small>durch den Würfel</small>
-</h1>
+<header>
+  <h1 class="m-4 text-center text-2xl font-bold">
+    Neunhundert neun und neunzig <br /> <small>und noch etliche</small> <br /> Almanachs-Lustspiele
+    <br /> <small>durch den Würfel</small>
+  </h1>
 
-<button
-  on:click={() => {
-    document.location.hash = wavyPath.join("")
-  }}>Wavy path</button
->
-<button
-  on:click={() => {
-    document.location.hash = generateRandomSequence().join("")
-  }}>Random path</button
->
+  <button
+    on:click={() => {
+      document.location.hash = wavyPath.join("")
+    }}>Wavy path</button
+  >
+  <button
+    on:click={() => {
+      document.location.hash = generateRandomSequence().join("")
+    }}>Random path</button
+  >
+</header>
 
-<!-- Display all six versions side by side -->
-<div class="versions">
-  {#each Array.from(new Array(200), (_x, i) => i + 1) as index}
-    <h2 id={index.toString()}>{index}</h2>
-    <div class="section">
-      {#each Array.from(new Array(6), (_x, i) => i + 1) as pips}
-        <div class="roll">
-          <h3 style:background={Number(sequence[index - 1]) === pips ? "goldenrod" : ""}>
-            {"●".repeat(pips)}
-          </h3>
-          <Segment {index} {pips} segments={data.segments} diceChart={data.diceChart} />
+<main class="p-4">
+  <!-- Display all six versions side by side -->
+  <div class="versions flex flex-col gap-10">
+    {#each Array.from(new Array(200), (_x, i) => i + 1) as index}
+      <div>
+        <h2 class="text-center text-xl font-bold" id={index.toString()}>{index}</h2>
+        <div class="section flex gap-2">
+          {#each Array.from(new Array(6), (_x, i) => i + 1) as pips}
+            <div class="roll flex-1">
+              <h3
+                class="text-center"
+                style:background={Number(sequence[index - 1]) === pips ? "goldenrod" : ""}
+              >
+                {"●".repeat(pips)}
+              </h3>
+              <Segment {index} {pips} segments={data.segments} diceChart={data.diceChart} />
+            </div>
+          {/each}
         </div>
-      {/each}
-    </div>
-  {/each}
-</div>
-
-<style>
-  .section {
-    /* Display side by side */
-    flex-direction: row;
-    display: flex;
-    gap: 0.5rem;
-    margin-bottom: 5rem;
-  }
-  .roll {
-    flex: 1;
-  }
-  h1,
-  h2,
-  h3 {
-    text-align: center;
-  }
-</style>
+      </div>
+    {/each}
+  </div>
+</main>
