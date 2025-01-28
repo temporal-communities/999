a67b10279172f70f8ca6679af12405d14bdc9aa0
Viktor Illmer <1476338+v-ji@users.noreply.github.com>
2025-01-28 15:56:41 +0100

    Add Tailwind CSS (#7)
    * Add Tailwind CSS v4
    
    * Add prettier-plugin-tailwindcss
    
    * Migrate styles to Tailwind

--- src/lib/components/Segment.svelte
+++ src/lib/components/Segment.svelte
@@ -8,57 +8,18 @@
   let segmentId = diceChart[index - 1][pips - 1]
 </script>
 
-<div class="segment">
+<div class="segment rounded-md border border-stone-200 bg-amber-50 p-4 shadow-md">
   <!-- eslint-disable-next-line svelte/no-at-html-tags -->
-  {@html segments[segmentId]}
-  <span class="number">{segmentId}</span>
+  <div class="tei">{@html segments[segmentId]}</div>
+  <span class="number mt-4 block text-center text-xs">{segmentId}</span>
 </div>
 
 <style>
-  /* Border */
-  .segment {
-    background: #fffbeb;
-    border: 1px solid #e7e5e4;
-    border-radius: 0.5rem;
-    padding: 1rem 2rem;
-    box-shadow:
-      0 4px 6px -1px rgb(0 0 0 / 0.1),
-      0 2px 4px -2px rgb(0 0 0 / 0.1);
-  }
-
-  .number {
-    display: block;
-    text-align: center;
-    opacity: 0.5;
-    font-size: small;
-    font-weight: normal;
-    margin-top: 1rem;
-  }
-
+  /* We need to apply this globally, as the elements are generated at runtime */
   :global([subtype*="manicule-left"] + .number:after) {
     content: " ☚";
   }
   :global([subtype*="manicule-right"] + .number:before) {
     content: "☛ ";
-  }
-
-  /* TEI styles */
-  :global(.tei-speaker) {
-    font-weight: bold;
-    /* Sperrsatz */
-    letter-spacing: 0.1em;
-  }
-  :global(.tei-stage) {
-    font-style: italic;
-  }
-  :global(.tei-sp) {
-    --indent: 1.5em;
-    text-indent: calc(var(--indent) * -1);
-    padding-inline-start: var(--indent);
-  }
-  :global(ul.tei-castList) {
-    list-style: none;
-    padding: 0;
-    margin-top: 0;
   }
 </style>
