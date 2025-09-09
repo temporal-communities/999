da9844c67383be9f66f59cf54f2543aa7cbd41fb
Viktor Illmer <1476338+v-ji@users.noreply.github.com>
2025-09-09 16:38:46 +0200

    Correct ESLint flat config rules placement

--- eslint.config.js
+++ eslint.config.js
@@ -19,9 +19,6 @@ export default ts.config(
       globals: {
         ...globals.browser,
         ...globals.node
-      },
-      rules: {
-        "@typescript-eslint/no-unused-vars": "off"
       }
     },
     rules: {
